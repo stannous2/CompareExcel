6a9c01c905ea8302cc776d85a7a3f32187936af4
stannous2 <33492860+stannous2@users.noreply.github.com>
2018-12-12 15:11:09 -0800

    2nd commit

--- app/scripts/loadExcelFile.js
+++ app/scripts/loadExcelFile.js
@@ -1,19 +1,50 @@
-//Read a file
-var workbook = new Excel.Workbook();
-workbook.xlsx.readFile("data/Sample.xlsx").then(function () {
-            
-//Get sheet by Name
-var worksheet=workbook.getWorksheet('Sheet1');
-            
-//Get Lastrow
-var row = worksheet.lastRow
-            
-//Update a cell
-row.getCell(1).value = 5;
- 
-row.commit();
- 
-//Save the workbook
-return workbook.xlsx.writeFile("data/Sample.xlsx");
- 
-});
+function readCsvFile(workbook, worksheet) {
+    //Read a file
+    var workbook = new Excel.Workbook();
+    workbook.xlsx.readFile("data/Sample.xlsx").then(function () {
+
+        //Get sheet by Name
+        var worksheet = workbook.getWorksheet('Sheet1');
+
+        //Get Lastrow
+        var row = worksheet.lastRow
+
+        //Update a cell
+        row.getCell(1).value = 5;
+
+        row.commit();
+
+        //Save the workbook
+        return workbook.xlsx.writeFile("data/Sample.xlsx");
+
+    });
+}
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+        f.size, ' bytes, last modified: ',
+        f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+        '</li>');
+    }
+    // document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+    $('#list').val(output.join(''));
+  }
+
+  function selectFile(id) {
+    console.log('label id: ', id);
+
+    // document.getElementById('custom-file-input').click();
+    $('#' + id).click()
+  }
+
+  function fileSelected(input, id) {
+    console.log('id: ', id)
+    console.log("input: ", input.value);
+
+    document.getElementById(id).value = input.value.split('\\').pop().split('/').pop();
+  }
