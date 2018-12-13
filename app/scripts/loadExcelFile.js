3c80f4af95b7c7f9052faebb33ef49e927abf881
stannous2 <33492860+stannous2@users.noreply.github.com>
2018-12-13 15:18:28 -0800

    update

--- app/scripts/loadExcelFile.js
+++ app/scripts/loadExcelFile.js
@@ -20,6 +20,17 @@ function readCsvFile(workbook, worksheet) {
     });
 }
 
+$('#input-excel').change(function(e){
+  var reader = new FileReader();
+  reader.readAsArrayBuffer(e.target.files[0]);
+  reader.onload = function(e) {
+          var data = new Uint8Array(reader.result);
+          var wb = XLSX.read(data,{type:'array'});
+          var htmlstr = XLSX.write(wb,{sheet:"Sheet1", type:'binary', bookType:'html'});
+          $('#wrapper')[0].innerHTML += htmlstr;
+  }
+});
+
 function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
 
